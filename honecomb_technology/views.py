e960f1539115210f07e83cff97546eb2a15511ca
yonis mohamoud <yonis838@googlemail.com>
2021-02-07 19:54:29 +0000

    Honeycomb new website

---
+++ honecomb_technology/views.py
@@ -0,0 +1,43 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from honecomb_technology import app
+
+@app.route('/')
+
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Welcome | Honeycomb Technology'
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact | Honeycomb Technology'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About | Honeycomb Technology'
+    )
+
+
+
+@app.route('/services')
+def services():
+    """Renders the about page."""
+    return render_template(
+        'services.html',
+        title='Services | Honeycomb Technology'
+    )
