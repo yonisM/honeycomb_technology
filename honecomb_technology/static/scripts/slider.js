ec2fb0f0557d5b5f45a864a20a2183e784b38ad0
yonis mohamoud <yonis838@googlemail.com>
2021-02-24 21:30:30 +0000

    more changes to homepage

--- honecomb_technology/static/scripts/slider.js
+++ honecomb_technology/static/scripts/slider.js
@@ -14,21 +14,19 @@ $(document).ready(function () {
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 2
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                     slidesToScroll: 1
                 }
             }
