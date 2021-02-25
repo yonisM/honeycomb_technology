1735149c8c776bc9c1324de46594c28e004acb56
yonis mohamoud <yonis838@googlemail.com>
2021-02-25 17:12:10 +0000

    changes to content security policy 2

--- honecomb_technology/__init__.py
+++ honecomb_technology/__init__.py
@@ -7,6 +7,16 @@ from flask_talisman import Talisman
 
 app = Flask(__name__)
 
-Talisman(app)
+csp = {
+    'default-src': [
+        '\'self\'',
+        '\'unsafe-inline\'',
+        'stackpath.bootstrapcdn.com',
+        'code.jquery.com',
+        'cdn.jsdelivr.net',
+        'fonts.googleapis.com'
+    ]
+
+Talisman(app, content_security_policy=csp)
 
 import honecomb_technology.views
